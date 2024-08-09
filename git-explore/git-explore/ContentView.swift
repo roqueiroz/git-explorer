345dc6cba077e5d65a2f1a9ec97b5f6bbabfe2c5
Rodrigo Queiroz <rodrigogqueiroz@live.com>
2024-08-09 17:45:19 -0300

    Create new project

---
+++ git-explore/git-explore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  git-explore
+//
+//  Created by Rodrigo Queiroz on 09/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ git-explore/git-explore/git_exploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_exploreApp.swift
+//  git-explore
+//
+//  Created by Rodrigo Queiroz on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct git_exploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
